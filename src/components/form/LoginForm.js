31441a20ed6f35287c6dbd74d903f46273d313c4
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2021-04-16 17:27:51 +0200

    forms hoc implemented

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,124 +1,85 @@
-import React from 'react';
+import { withFormValidation }  from './withFormValidation';
 import { withRouter, Link } from 'react-router-dom';
-import InputField from './InputField';
-import ButtonPrimary from './ButtonPrimary';
-import {api, handleError} from "../../helpers/api";
+import { api } from "../../helpers/api";
+import { ServerError, ButtonPrimary, ValidatedInput} from "../../helpers/formUtils"
 import User from "../shared/models/User";
 
-class LoginForm extends React.Component {
+const LoginFormSkeleton = (props) => {
+  const {
+    onFormValueChange,
+    onFormSubmit,
+    values,
+    fieldErrors,
+    serverError
+  } = props;
 
-	constructor() {
-		super();
-		this.state = {
-            usernameOrEmail: '',
-            password: '',
-            errorMessageUsernameOrEmail: null,
-            errorMessagePassword: null,
-            isUsernameOrEmailValid: true,
-            isPasswordValid: true,
-            serverError: null
-        };
-        this.submit = this.submit.bind(this);
-        this.isFormValid = this.isFormValid.bind(this);
-        this.login = this.login.bind(this);
-    }
-
-    submit(resolve) { 
-        let usernameOrEmailValue = this.state.usernameOrEmail;
-        let passwordValue = this.state.password;
-
-        if(this.isFormValid(usernameOrEmailValue, passwordValue)) {
-            this.login(resolve, usernameOrEmailValue, passwordValue)
-        } else {
-			resolve();
-		}
-    }
-
-    isFormValid(usernameOrEmailValue, passwordValue) {
-        let isFormValid = true;
-        this.setState({
-            isUsernameOrEmailValid: true,
-            isPasswordValid: true,
-            serverError: null
-        });
-
-        if(!usernameOrEmailValue || usernameOrEmailValue === '') {
-			this.setState({
-				errorMessageUsernameOrEmail: 'Please fill in this field',
-				isUsernameOrEmailValid: false,
-            });
-            isFormValid = false;
-		}
-        if(!passwordValue || passwordValue === '') {
-			this.setState({
-				errorMessagePassword: 'Please fill in this field',
-				isPasswordValid: false,
-            });
-            isFormValid = false;
-        } 
-
-        return isFormValid;
-    }
-
-    async login(resolve, usernameOrEmailValue, passwordValue){
-        try{
-            const requestBody = JSON.stringify({
-                usernameOrEmail: usernameOrEmailValue,
-                password: passwordValue
-            });
-            const response = await api.post(`/users/login`, requestBody);
-            // Get the returned user and update a new object.
-            const user = new User(response.data);
-
-            // Store the token into the local storage.
-            localStorage.setItem('token', user.token);
-
-            // Login successfully worked --> navigate to the route /game in the GameRouter
-            this.props.history.push(`/home`);
-        } catch (error) {
-            this.setState({
-                serverError: handleError(error)
-            })
-        }
-        finally {
-            // return resolve after having received a response from the api
-            resolve();
-        }
-    }
-
-    // TODO optional: add link "forgot password?"
-	render() {
-		return (
-			<div className="login-form">
-                {this.state.serverError ? 
-                    <p className="server-error">{this.state.serverError}</p> : null
-                }
-				<InputField	
-                    type="text"
-                    label="Username or email"
-                    placeholder="Username or email"
-                    value={this.state.usernameOrEmail}
-                    isValid={this.state.isUsernameOrEmailValid}	
-                    errorMessage={this.state.errorMessageUsernameOrEmail}
-                    onChange={(e) => this.setState({usernameOrEmail: e.target.value})}	
-                />
-                <InputField	
-                    type="password"
-                    label="Password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    isValid={this.state.isPasswordValid}	
-                    errorMessage={this.state.errorMessagePassword}
-                    onChange={(e) => this.setState({password: e.target.value})}	
-                />
-                <ButtonPrimary 
-                    value="Sign in" 
-                    submitFunction={this.submit}
-                />
-                <p className="below-btn">Don't have an account? <Link to="/register">Sign up</Link></p>
-			</div>
-		);
-	}
+  return (
+    <div className="login-form">
+        <ServerError serverError={serverError}/>
+        <ValidatedInput 
+            type="text"
+            label="Username or email"
+            placeholder="Username or email"
+            error={fieldErrors.usernameOrEmail}
+            onChange={onFormValueChange} 
+            value={values.usernameOrEmail}
+            name="usernameOrEmail"
+        />
+        <ValidatedInput 
+            type="password"
+            label="Password"
+            placeholder="Password"
+            error={fieldErrors.password}
+            onChange={onFormValueChange} 
+            value={values.password}
+            name="password"
+        />
+        <ButtonPrimary 
+            value="Sign in" 
+            onClick={onFormSubmit}
+        />
+        <p className="below-btn">Don't have an account? <Link to="/register">Sign up</Link></p>
+    </div>
+  )
 }
+
+const initialValues = {
+  usernameOrEmail: '',
+  password: ''
+};
+
+const rules = {
+  usernameOrEmail: [
+    [value => value != '', 'Fill in a username or email'],
+  ],
+  password: [
+    [value => value != '', 'Fill in a password'],
+  ]
+};
+
+const handlers = {
+  // login on form submit
+  onFormSubmit: async (values) => {
+    try {
+      const requestBody = JSON.stringify({
+          usernameOrEmail: values.usernameOrEmail,
+          password: values.password
+      });
+      const response = await api.post(`/users/login`, requestBody);
+      // Get the returned user and update a new object.
+      const user = new User(response.data);
+
+      // Store the token into the local storage.
+      localStorage.setItem('token', user.token);
+
+      // Login successfully worked --> navigate to the route /game in the GameRouter
+      this.props.history.push(`/home`);
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+const LoginForm = withFormValidation(initialValues, rules, handlers, LoginFormSkeleton);
 
 export default withRouter(LoginForm);
